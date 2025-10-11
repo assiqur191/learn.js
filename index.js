add3a5cdc6c5646ad50541e5b3ed5e18f2f388b6
unknown <rahmanmdassiqur@gmail.com>
2025-10-12 03:26:03 +0600

    mod-8

---
+++ index.js
@@ -0,0 +1,45 @@
+// console.log("Hello world");
+// //variable
+// var x = 20;
+// let y = 15;
+// const z =35;
+
+// console.log (x);
+
+// //primitive type
+// let name = "jhon"; //string
+// let age = 25; //int
+// let isActive = true;//bolian
+// let h ; // undefine
+// let v = null//null
+
+// console.log(isActive);
+
+// // non primitive type
+ 
+// let colors = ["red","orange","blue"]
+
+//oparator 
+
+// let a= 20;
+// let b = 15;
+
+// console.log(a + b )
+// console.log(a - b )
+// console.log(a * b )
+// console.log(a / b )
+// console.log(a % b )
+// console.log(a ** b )// exponential
+
+//for loop
+// let x = 1;
+// for (x = 1;x <= 100; x++){
+//     console.log(`9*${x} = ${9*x}`)
+// }
+
+// while loop
+let x=1;
+while (x <= 10){
+    console.log(`9*${x} = ${9*x}`)
+    x++;
+}
