39ac995254ff6b904a5b24e615e444febc1eaf5d
unknown <rahmanmdassiqur@gmail.com>
2025-10-25 02:45:09 +0600

    module9 first class edit

--- module9.js
+++ module9.js
@@ -1,11 +1,11 @@
 // #javascript object & jason--------------------------------------->
 
-let person = {
-  firstname: "sayem",
-  lastname: "rahman",
-  age: 32,
-  isdeveloper: true,
-};
+// let person = {
+//   firstname: "sayem",
+//   lastname: "rahman",
+//   age: 32,
+//   isdeveloper: true,
+// };
 // console.log(person);
 // console.log(person["firstname"]);
            // add in the object
@@ -25,23 +25,45 @@ let person = {
       //---------->#jason<-------------
       //( 1,inside a API json make comunication with forntend and backend )(js object notation= json)
          
-      let student= {
-        name:"sagor",
-        age : 50,
-        country : "bidesh",
-      }
-    //   console.log(student);
+    //   let student= {
+    //     name:"sagor",
+    //     age : 50,
+    //     country : "bidesh",
+    //   }
+    // //   console.log(student);
 
-    let studentjson = JSON.stringify(student);
-    console.log(studentjson);
-    let datajson= JSON.parse(studentjson);
-    console.log(datajson); 
+    // let studentjson = JSON.stringify(student);
+    // console.log(studentjson);
+    // let datajson= JSON.parse(studentjson);
+    // console.log(datajson); 
 
 
 
 // #js array
 // #strings and string operatons.
 // #javascript data
-// #math, Number ,
+//<------------------------------------------------------- #math, Number ,
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.abs(-10));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.6));
+// console.log(Math.round(4.8));
+// console.log(Math.max(10, 20, 50));
+// console.log(Math.min(10, 40, 60));
+// console.log(Math.random());
+// let randomnum = Math.round(Math.random()*100) + 1;
+// console.log(randomnum);
+
 // #window object,
+  
+
 // #navigator object
+
+//<----------------------------------------------------------------------------------------------------------------------------------------------------->
+//<----------------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+
+
+
